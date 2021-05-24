cca7b0910788b2aa6fdb8176c26d642f312fc23f
Lam Nhan <lamnhan92@gmail.com>
2021-05-24 21:06:17 +0700

    add blank comps and pages

---
+++ projects/dashboard/src/lib/pages/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco';
+import { RouterLinkDirectiveModule } from '@lamnhan/ngx-useful';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    TranslocoModule,
+    RouterLinkDirectiveModule,
+    HomeRoutingModule
+  ]
+})
+export class HomePageModule {}
--- projects/dashboard/src/public-api.ts
+++ projects/dashboard/src/public-api.ts
@@ -3,3 +3,9 @@
  */
 
 export * from './lib/dashboard.module';
+
+export * from './lib/components/header/header.component';
+export {HeaderComponentModule} from './lib/components/header/header.module';
+
+export * from './lib/pages/home/home.component';
+export {HomePageModule} from './lib/pages/home/home.module';
