920c4beafd8006173548a339d303a57d830ef165
Lam Nhan <lamnhan92@gmail.com>
2021-07-17 12:39:59 +0700

    update media page

--- projects/dashboard/src/lib/pages/media/media.component.ts
+++ projects/dashboard/src/lib/pages/media/media.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { map, tap } from 'rxjs/operators';
 
 import { UploadResult } from '../../services/storage/storage.service';
+
+import { GetMedia, MediaItem } from '../../states/media/media.state';
 
 @Component({
   selector: 'nguix-dashboard-media-page',
@@ -8,14 +13,43 @@ import { UploadResult } from '../../services/storage/storage.service';
   styleUrls: ['./media.component.scss']
 })
 export class MediaPage implements OnInit {
+
+  public readonly page$ = this.route.params.pipe(
+    map(params => {
+      return { ok: true };
+    }),
+    tap(() => {
+      this.store.dispatch(new GetMedia(true));
+    }),
+  );
+
+  public readonly data$ = this.store
+    .select(state => state.media)
+    .pipe(
+      map(media => {
+        const listingItems = this.buildListingItems(media.files || []);
+        console.log(listingItems);
+        return {
+          listingItems,
+        };
+      }),
+    );
+
   showUploader = false;
+  query = '';
 
-  constructor() {}
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   uploadComplete(result: UploadResult) {
     console.log(result);
+  }
+
+  private buildListingItems(files: MediaItem) {
+    return files;
   }
 }
--- projects/dashboard/src/lib/states/media/media.state.ts
+++ projects/dashboard/src/lib/states/media/media.state.ts
@@ -2,14 +2,22 @@ import { Injectable } from '@angular/core';
 import { of, combineLatest } from 'rxjs';
 import { tap, map, catchError } from 'rxjs/operators';
 import { State, Action, StateContext } from '@ngxs/store';
+import { AngularFireStorageReference } from '@angular/fire/storage';
+
+import { StorageService } from '../../services/storage/storage.service';
 
 export interface MediaStateModel {
-  files: any[];
+  files: MediaItem[];
+}
+
+export interface MediaItem {
+  path: string;
+  ref: AngularFireStorageReference;
 }
 
 export class GetMedia {
-  static readonly type = '[Media] Get all items';
-  constructor() {}
+  static readonly type = '[Media] Get all files';
+  constructor(public refresh = false) {}
 }
 
 @State<MediaStateModel>({
@@ -21,11 +29,35 @@ export class GetMedia {
 @Injectable()
 export class MediaState {
 
-  constructor() {}
+  constructor(private storageService: StorageService) {}
 
   @Action(GetMedia)
   getMedia({ getState, patchState }: StateContext<MediaStateModel>, action: GetMedia) {
     const state = getState();
+    const { refresh } = action;
+    if (state.files.length) {
+      const { files } = state;
+      if (refresh) {
+        patchState({files});
+      }
+      return of(files);
+    } else {
+      return this.storageService.list().pipe(
+        map(listResult => {
+          const { items } = listResult;
+          return items.map(item => ({
+            path: item.fullPath,
+            ref: this.storageService.ref(item.fullPath),
+          }));
+        }),
+        catchError(() => {
+          return of([] as MediaItem[]);
+        }),
+        tap(files => {
+          patchState({files});
+        }),
+      );
+    }
   }
 
 }
