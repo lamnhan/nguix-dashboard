a58c6f1ede415d890bc563da3a3476bb6a91891b
Lam Nhan <lamnhan92@gmail.com>
2021-07-20 14:39:49 +0700

    update media

--- projects/dashboard/src/lib/components/uploader/uploader.module.ts
+++ projects/dashboard/src/lib/components/uploader/uploader.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslocoModule } from '@ngneat/transloco';
+import { NgxFilesizeModule } from 'ngx-filesize';
 import { RouterLinkDirectiveModule } from '@lamnhan/ngx-useful';
 
 import { UploaderComponent } from './uploader.component';
@@ -12,6 +13,7 @@ import { UploaderComponent } from './uploader.component';
     CommonModule,
     FormsModule,
     TranslocoModule,
+    NgxFilesizeModule,
     RouterLinkDirectiveModule,
   ],
   exports: [UploaderComponent]
